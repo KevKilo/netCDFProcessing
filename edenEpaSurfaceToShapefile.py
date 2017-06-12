b79bf2d19573dc1c8e80d4ae724ae8a793c9c993
Kevin Kotun <Kevin_Kotun@nps.gov>
2017-06-12 15:07:42 -0400

    Updated code to python3 added argument parser -d yyyy-mm-dd is optional if no date, then a shapefile for all dates in the netCdf will be generated.

--- edenEpaSurfaceToShapefile.py
+++ edenEpaSurfaceToShapefile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #
 # create a depth/stage shapefile for a specific day OR every day in the eden netCDF file
@@ -13,6 +13,7 @@
 
 from osgeo import osr
 from osgeo import ogr
+from osgeo import gdal
 import sys
 import string
 import os
@@ -20,198 +21,208 @@ import subprocess
 import numpy
 import datetime
 import time
-import Scientific.IO.NetCDF
-from Scientific.IO.NetCDF import NetCDFFile
+from argparse import ArgumentParser
+from scipy.io import netcdf
+import subprocess
 
-debug = True
+def main():
 
-if len(sys.argv) <> 3:
-    sys.exit("\nUsage: %s eden_netCDF_filename date OR \"all\" \n\n" % sys.argv[ 0 ] )
+    args = parseCmdLine()
 
+    if args.verbose: print( __name__ )
+    if args.verbose: print( args )
+
+#   this is probably not need as I am not sure exactly what it does
 #
+    gdal.UseExceptions()
+
 # netCDF_fileName is the quarterly netCDF file from eden
-
-netCDF_fileName = sys.argv[ 1 ]
-surfaceDateString = sys.argv[ 2 ]
-
-surfaceDateInFile = False
-allDays = True
+#
+    surfaceDateInFile = False
 
 # Determine if we are running all days in the netCDF File
 # or just one day
 #
+    if args.surfaceDateString:
 
-if surfaceDateString.upper() != "ALL":
+        allDays = False
+        date_field = args.surfaceDateString.split( '-' ) 
 
-    allDays = False
-       
-    date_field = surfaceDateString.split( '-' ) 
+        year = date_field[ 0 ]
+        month = date_field[ 1 ]
+        day = date_field[ 2 ]
+        surfaceDate = datetime.date( int( year ), int( month ), int( day ) )
 
-    year = date_field[ 0 ]
-    month = date_field[ 1 ]
-    day = date_field[ 2 ]
-    
-    surfaceDate = datetime.date( int( year ), int( month ), int( day ) )
+    else:
+        surfaceDate = None
+        allDays = True
 
-dataDir = "/physical/agency_data/eden"
-gis_dir = "/physical/gis/eden"
+    dataDir = "/physical/agency_data/eden"
+    gisDir = "/physical/gis/eden"
 
 #	lsel netcdf hardcoded b/c it rarely changes
-
-netcdf_lsel_filename = dataDir + "/dem/eden_dem_cm_oc11.nc"
+#
+    netcdf_lsel_filename = dataDir + "/dem/eden_dem_cm_oc11.nc"
 
 #	open netCDF files
+#
+    if args.verbose: print ( "netcdf stage file: %s" % args.netCDF_fileName )
+    stage_input = netcdf.netcdf_file( args.netCDF_fileName, 'r' )
 
-print "netcdf stage file: %s" % netCDF_fileName
-stage_input = NetCDFFile( netCDF_fileName, 'r' )
+    if args.verbose: print ( "netcdf lsel file: %s" % netcdf_lsel_filename )
+    lsel_input = netcdf.netcdf_file( netcdf_lsel_filename, 'r' )
 
-print "netcdf lsel file: %s" % netcdf_lsel_filename
-lsel_input = NetCDFFile( netcdf_lsel_filename, 'r' )
+    depth = numpy.zeros( ( 405, 287 ), 'f' )
 
-depth = numpy.zeros( ( 405, 287 ), 'f' )
+    stage = stage_input.variables[ 'stage' ][ : ]
+    lsel = lsel_input.variables[ 'dem' ][ : ]
+    ntime,nrowStage, ncolStage = stage.shape
+    nrowLsel, ncolLsel = lsel.shape
 
-stage = stage_input.variables[ 'stage' ][ : ]
-lsel = lsel_input.variables[ 'dem' ][ : ]
-ntime,nrowStage, ncolStage = stage.shape
-nrowLsel, ncolLsel = lsel.shape
-
-if nrowStage != nrowLsel or ncolStage != ncolLsel:
-    print "Warning lsel shape and stage shape are NOT the same"
-    print "stage nrow: %s ncol: %s" % ( nrowStage, ncolStage )
-    print " lsel nrow: %s nclo: %s" % ( nrowLsel, ncolLsel )
+    if nrowStage != nrowLsel or ncolStage != ncolLsel:
+        print ( "Warning lsel shape and stage shape are NOT the same" )
+        print ( "stage nrow: %s ncol: %s" % ( nrowStage, ncolStage ) )
+        print ( " lsel nrow: %s nclo: %s" % ( nrowLsel, ncolLsel ) )
 
 #
 # Get a python date from the netCDF time attribute: time.units
 #
+    netCdfStageDate = stage_input.variables[ 'time' ]
 
-stage_date = stage_input.variables[ 'time' ]
+    dateUnitString = netCdfStageDate.units.decode() 
 
-dateUnitString = stage_date.units
+    dateUnitStringField =  dateUnitString.split()
 
-dateUnitStringField =  dateUnitString.split()
+    iso_datetime = dateUnitStringField[ 2 ]
 
-iso_datetime = dateUnitStringField[ 2 ]
+    date_time_field = str( iso_datetime).split('T')
 
-date_time_field = iso_datetime.split('T')
+    date_field = date_time_field[ 0 ].split( '-' )
 
-date_field = date_time_field[ 0 ].split( '-' )
+    year = date_field[ 0 ]
+    month = date_field[ 1 ]
+    day = date_field[ 2 ]
 
-year = date_field[ 0 ]
-month = date_field[ 1 ]
-day = date_field[ 2 ]
-
-netCdfStartDate = datetime.date( int( year ), int( month ), int( day ) )
+    netCdfStartDate = datetime.date( int( year ), int( month ), int( day ) )
 
 # for each day in the netCdf file...
 #
+    netCdfDateList = []
+    for day in range( 0, ntime ):
 
-for day in range( 0, ntime ):
+        day_offset = datetime.timedelta( days = day )
+        netCdfSurfaceDate = netCdfStartDate + day_offset
 
-    day_offset = datetime.timedelta( days = day )
-
-    stage_surface_date = netCdfStartDate + day_offset
+        if args.verbose: netCdfDateList.append( netCdfSurfaceDate )
 
 # figure out if we are doing just one day or all days in the 
 # netCDF file
-#
-    if allDays == False:
-        if stage_surface_date != surfaceDate:
-            if debug: print stage_surface_date
-            continue
-        else:
-            surfaceDateInFile = True
+# 
+        if args.surfaceDateString: 
 
+            if netCdfSurfaceDate != surfaceDate:
+                continue
+            else:
+                surfaceDateInFile = True
 
-    dateStamp = "%d%02d%02d" % ( stage_surface_date.year, stage_surface_date.month, stage_surface_date.day )
-    iso_date = "%d-%02d-%02d" % ( stage_surface_date.year, stage_surface_date.month, stage_surface_date.day )
+        dateStamp = "%d%02d%02d" % ( netCdfSurfaceDate.year, netCdfSurfaceDate.month, netCdfSurfaceDate.day )
+        iso_date = "%d-%02d-%02d" % ( netCdfSurfaceDate.year, netCdfSurfaceDate.month, netCdfSurfaceDate.day )
 
 # For each column for each row
 # calc depth from stage and lsel
 #
 
-    for i in range( 0, nrowStage ):
-        for j in range( 0, ncolStage ):
+        for i in range( 0, nrowStage ):
+            for j in range( 0, ncolStage ):
 
-            if numpy.isnan( lsel[ i, j ] ): 
-                continue
+                if numpy.isnan( lsel[ i, j ] ): 
+                    continue
 
-            depth[ i, j ] = ( stage[ day, i, j ] - lsel[ i, j ] )
+                depth[ i, j ] = ( stage[ day, i, j ] - lsel[ i, j ] )
 #
 # Create and Define the Spatial Reference
 
-    if debug: print "Creating Spatial Reference"
+        if args.verbose: print ( "Creating Spatial Reference" ) 
 
-    spatialReference = osr.SpatialReference() 
-    spatialReference.ImportFromProj4('+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m')
+        spatialReference = osr.SpatialReference() 
+        spatialReference.ImportFromProj4('+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m')
 
 #
 # Establish Driver for Shapefile
 
-    if debug: print "creating driver"
+        if args.verbose: print ( "creating driver" ) 
 
-    driverName =  "ESRI Shapefile"
+        driverName =  "ESRI Shapefile"
 	
-    driver = ogr.GetDriverByName( driverName )
+        driver = ogr.GetDriverByName( driverName )
 
-    if driver is None:
-        print "Driver Not Available: %s" % driverName
-        sys.exit( 1 )
+        if driver is None:
+            print ( "Driver Not Available: %s" % driverName ) 
+            sys.exit( 1 )
 
-    if debug: print "creating dataSource"
+        if args.verbose: print ( "creating dataSource" ) 
 
-    path = "/physical/gis/eden/"
-    dataSource = driver.CreateDataSource( path )
+        dataSource = driver.CreateDataSource( gisDir )
 
-    if dataSource is None:
-        print "Creation of DataSource output file FAILED"  
-        sys.exit( 1 )
+        if dataSource is None:
+            print  ( "Creation of DataSource output file FAILED"   ) 
+            sys.exit( 1 )
 
-    if debug: print "creating Layer"
+        if args.verbose: print ( "creating Layer" ) 
 
-#
 # GIS Section
 #
 # Create the Layer
 # This will be the name of the shapefile that is written to the DataSource Directory
 #
 
-    layer_name = "eden_epa" + dateStamp
+        layer_name = "eden_epa" + dateStamp
 
-#
 # delete file if it exists
-	
-    pathFileName =  gis_dir + "/" + layer_name + ".shp"
-
-    if os.path.isfile( pathFileName ):
-
-        print "File exists, must be deleted: %s" % pathFileName
-        systring = "rm " + gis_dir + "/" + layer_name + ".*"
-        if ( os.system( systring ) ) == 0: 
-            print "file deleted"
-        else: print "could not delete file"
-
-    print "Creating: %s\n" % pathFileName
-    grid_layer = dataSource.CreateLayer( layer_name, spatialReference, ogr.wkbMultiPolygon )
-
 #
+        pathFileName =  gisDir + "/" + layer_name + ".shp"
+
+        if os.path.isfile( pathFileName ):
+
+            print ( "File exists, must be deleted: %s" % pathFileName ) 
+
+##            cmd = [ 'rm', pathFileName ]
+##            if ( subprocess.call( cmd ) ) == 0:
+
+            fileSpecification = gisDir + '/' + layer_name + '.*'
+            systring = 'rm ' +  fileSpecification  
+            if subprocess.call( systring, shell=True ) == 0:
+                print ( "file deleted" ) 
+            else: print ( "could not delete file" ) 
+
+        if args.verbose: print ( "Creating: %s\n" % pathFileName ) 
+
+        try:
+            grid_layer = dataSource.CreateLayer( layer_name, spatialReference, ogr.wkbMultiPolygon )
+        except NameError:
+            print( 'Creations of grid_layer Failed Miserably' )
+            raise
+
+        print ( type( grid_layer )  )
+        print ( "grid_layer" ) 
+
 # Add fields to the layer
 # ogr.FieldDefn return instances of the class osgeo.ogr.FieldDefn
 #
 
-    field_row = ogr.FieldDefn( "row", ogr.OFTInteger )
-    field_col = ogr.FieldDefn( "col", ogr.OFTInteger )
-    field_Stage = ogr.FieldDefn( "Stage", ogr.OFTReal )
-    field_WaterDepth = ogr.FieldDefn( "WaterDepth", ogr.OFTReal )
+        field_row = ogr.FieldDefn( "row", ogr.OFTInteger )
+        field_col = ogr.FieldDefn( "col", ogr.OFTInteger )
+        field_Stage = ogr.FieldDefn( "Stage", ogr.OFTReal )
+        field_WaterDepth = ogr.FieldDefn( "WaterDepth", ogr.OFTReal )
 
 
-# 
 # Create the fields in the layer from the FieldDefns above
 #
 
-    grid_layer.CreateField( field_row )
-    grid_layer.CreateField( field_col )
-    grid_layer.CreateField( field_WaterDepth )
-    grid_layer.CreateField( field_Stage )
+        grid_layer.CreateField( field_row )
+        grid_layer.CreateField( field_col )
+        grid_layer.CreateField( field_WaterDepth )
+        grid_layer.CreateField( field_Stage )
 
 # dont think I need this. The layer is a multipolygon already
 # multipolygon = ogr.Geometry( ogr.wkbMultiPolygon )
@@ -219,66 +230,116 @@ for day in range( 0, ntime ):
 #
 # get the layer def to create the features
 #
-    grid_layer_defn = grid_layer.GetLayerDefn()
+        grid_layer_defn = grid_layer.GetLayerDefn()
 
-    if debug: print "creating Geometry"
+        if args.verbose: print ( "creating Geometry" ) 
 
 ##left_edge_x = 476800
-    left_edge_x = 463200
-    bottom_edge_y = 2790000
+        left_edge_x = 463200
+        bottom_edge_y = 2790000
 
-    start_y = bottom_edge_y
+        start_y = bottom_edge_y
 
-    for i in range( 0, nrowStage ):
+        for i in range( 0, nrowStage ):
 
 #	offset start_x 400m to west which is cancelled out in next loop
 #
-        start_x = left_edge_x - 400
-        start_y = bottom_edge_y + ( i * 400 )
+            start_x = left_edge_x - 400
+            start_y = bottom_edge_y + ( i * 400 )
 
-        for j in range( 0, ncolStage ):
+            for j in range( 0, ncolStage ):
 
-            start_x += 400
+                start_x += 400
 
-            if numpy.isnan( lsel[ i, j ] ): 
-                continue
+                if numpy.isnan( lsel[ i, j ] ): 
+                    continue
 
-            feature = ogr.Feature( grid_layer_defn )
+                feature = ogr.Feature( grid_layer_defn )
 #
 #	This code would not work until I casted the depth as float
 #	But the rest of them worked fine, probably b/c they are ints
 #
 
-            feature.SetField( "WaterDepth", float( depth[ i, j ] ) ) 
-            feature.SetField( "Stage", float( stage[day, i, j ] ) ) 
-            feature.SetField( "row", i ) 
-            feature.SetField( "col", j ) 
+                feature.SetField( "WaterDepth", float( depth[ i, j ] ) ) 
+                feature.SetField( "Stage", float( stage[day, i, j ] ) ) 
+                feature.SetField( "row", i ) 
+                feature.SetField( "col", j ) 
 # 
 # Fields of the feature are set individually, then destroyed
 # the new one is created above with the same grid_layer_defn...
 # i think?
 
 
-            ring = ogr.Geometry( ogr.wkbLinearRing )
+                ring = ogr.Geometry( ogr.wkbLinearRing )
 
-            ring.AddPoint( start_x, start_y )
-            ring.AddPoint( start_x + 400, start_y )
-            ring.AddPoint( start_x + 400, start_y + 400 )
-            ring.AddPoint( start_x, start_y + 400 )
-            ring.AddPoint( start_x, start_y )
-            cell = ogr.Geometry( ogr.wkbPolygon )
-            cell.AddGeometry( ring )
+                ring.AddPoint( start_x, start_y )
+                ring.AddPoint( start_x + 400, start_y )
+                ring.AddPoint( start_x + 400, start_y + 400 )
+                ring.AddPoint( start_x, start_y + 400 )
+                ring.AddPoint( start_x, start_y )
+                cell = ogr.Geometry( ogr.wkbPolygon )
+                cell.AddGeometry( ring )
 	
-            feature.SetGeometry( cell )
+                feature.SetGeometry( cell )
 
 ## it seems that a shapefile provide this for free
 ##		featureIndex = ( ( i * j) + j ) 
 ##		feature.SetFID( featureIndex )
 
-            grid_layer.CreateFeature( feature )
-            feature.Destroy()
+                grid_layer.CreateFeature( feature )
+# guidance from here: https://trac.osgeo.org/gdal/wiki/PythonGotchas indicates
+#    that one should basically never use destroy.  It says it is not needed at
+#    all to mitigate for leaks b/c python takes care of it when it goes out of scope
+#    anyway. it suggested setting it to none if you really wanted to do something useful
+#    AND, serendipitously using the None made this error go away:
+#
+#               file exists, must be deleted: /physical/gis/eden/eden_epa20000105.shp
+#               file deleted
+#               ERROR 4: Unable to open /physical/gis/eden/eden_epa20000105.shp or /physical/gis/eden/eden_epa20000105.SHP.
+#               ERROR 4: Failed to open file /physical/gis/eden/eden_epa20000105.shp.
+#               It may be corrupt or read-only file accessed in update mode.
+#
+#                feature.Destroy()
+                feature = None
 
-if allDays == False and surfaceDateInFile == False:
-    sys.exit( "\nDate Entered ***** %s ***** is Not in the netCDF File\n" % surfaceDateString ) 
 
-dataSource.Destroy()
+    if allDays == False and surfaceDateInFile == False:
+        sys.exit( "\nDate Entered ***** %s ***** is Not in the netCDF File\n" % surfaceDateString ) 
+
+#    dataSource.Destroy()
+    dataSource = None
+
+    if args.verbose: print( netCdfDateList )
+
+#-----------------------------------------------------------------------------------------------------------
+#  Parse Command Line Args
+
+def parseCmdLine():
+
+    home_dir = os.getenv( 'HOME', default = os.getcwd() )
+
+    parser = ArgumentParser( description = "Eden netCDF to Shapefile" )
+
+    parser.add_argument( 'netCDF_fileName',
+                        type = str,
+                        action = 'store',
+                        help = 'netCDF File Name' )
+
+    parser.add_argument( '-d', '--date',
+                        dest = 'surfaceDateString',
+                        type = str,
+                        action = 'store', 
+                        help = 'date yyyy-mm-dd for shapefile output' )
+
+    parser.add_argument( '-v', '--verbose',
+                        action = 'store_true',
+                        help = 'to provide additional output' )
+
+    args = parser.parse_args()
+    return args
+
+#----------------------------------------------------------------------------
+# Provide for cmd line invocation: not executed on import
+
+if __name__ == '__main__' :
+    main()
